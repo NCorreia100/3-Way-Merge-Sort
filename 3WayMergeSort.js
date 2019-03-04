395398ca76a0f38e3d8c0f0d1b586123385528e5
Nuno <NCorreia1@my.devry.edu>
2019-03-04 09:58:16 -0500

    edited comments

--- 3WayMergeSort.js
+++ 3WayMergeSort.js
@@ -1,6 +1,6 @@
 const threeWayMergeSort = (array) => {
 
-    //after array is broken down, sort the elems using insertion sort and return array
+    //after array is broken down, sort the elems using insertion sort and return array 
     if (array.length <= 3) {
         let sortedArr = [array.shift()];
         while (array.length > 0) {
@@ -11,14 +11,14 @@ const threeWayMergeSort = (array) => {
         }
         return sortedArr;
     }
-    //divide array into 3 equal sections (maintaining 1/3 of the original array)    
+    //divide array into 3 equal sections (maintaining 1/3 of the original array)  O( 2/3N )  
     let initialSize = array.length, partialSize = Math.floor(array.length / 3)
     let arr1 = array.splice(0, partialSize), arr2 = array.splice(0, partialSize);
 
-    // //recursively sort the 3 sections
+    // //recursively sort the 3 sections O( logN )
     arr1 = threeWayMergeSort(arr1), arr2 = threeWayMergeSort(arr2), array = threeWayMergeSort(array);
 
-    //merge the 3 arrays through insert sort and by shifting the lowest value
+    //merge the 3 arrays through insert sort and by shifting the lowest value O( N )
     let sortedArr = [];
     for (let i = 0; i < initialSize; i++) {
         if (arr1[0] <= arr2[0] || arr2.length === 0) {
